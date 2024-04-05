34b6fd3ba2a5f9ee70d2f9952430fcfccfee7068
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2024-04-05 15:27:12 -0300

    [feat(bob's set)]: updated textures and tooltip

--- Armor/BobChestplate.cs
+++ Armor/BobChestplate.cs
@@ -1,17 +1,28 @@
 ﻿using Terraria;
 using Terraria.ID;
-using Terraria.ModLoader;   
+using Terraria.ModLoader;
 
 namespace MarksClothes.Armor
 {
     [AutoloadEquip(EquipType.Body)]
-    public class BobChestplate : ModItem
+    public class BobChestplate : ModItem // It's more of a Bob's Robe but i am not bothering to change it for now
     {
+        public override void Load()
+        {
+            EquipLoader.AddEquipTexture(Mod, $"{Texture}_{EquipType.Legs}", EquipType.Legs, this);
+            base.Load();
+        }
         public override void SetDefaults()
         {
             Item.value = 2000;
             Item.rare = ItemRarityID.Pink;
             base.SetDefaults();
+        }
+        public override void SetMatch(bool male, ref int equipSlot, ref bool robes)
+        {
+            robes = true;
+            base.SetMatch(male, ref equipSlot, ref robes);
+            equipSlot = EquipLoader.GetEquipSlot(Mod, Name, EquipType.Legs);
         }
 
         public override void UpdateEquip(Player player)
